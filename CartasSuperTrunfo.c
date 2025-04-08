954d5a9115ba1686e86c5b892bbc8ebb04779e29
jonnassouza1978 <jonnassouza@yahoo.com.br>
2025-04-08 16:58:22 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,19 +2,49 @@
 
 
 int main() {
+   char estado1[50]; santa catarina
    char codigo1[4]; A01
-   char cidade[50]; blumenau
+   char cidade1[50]; blumenau
    int populacao1; 31.621
    float pib1; 20.576.440    
-   int pontos turisticos; 30
+   int pontos turisticos1; 30
 
+   char estado[50]; bahia
    char codigo2[4]; B02
    char cidade[50]; salvador
    int populacao2; 2.418.000
-   float pib2; 63.526.092    
+   float pib2; 63.526.092,00    
    int pontos turisticos2; 30
+ 
+   printf("digite o estado1: \n");
+   scanf(" %c" , &santa catarina);
+   printf("digite o codigo1: \n");
+   scanf(" %c" , A01);
+   printf("digite a cidade1: \n");
+   scanf(" %c" , blumenau);
+   printf("digite a populacao1: \n");
+   scanf(" %d" , 31.621);
+   printf("digite o pib1: \n");
+   scanf(" %.2f" , 20.576.440,00);
+   printf("digite os pontos turisticos1: \n");
+   scanf(" %d" , 30);
 
-       
+   printf("digite o estado2: \n");
+   scanf(" %c" , &bahia);
+   printf("digite o codigo2: \n");
+   scanf(" %c" , B02);
+   printf("digite a cidade2: \n");
+   scanf(" %c" , salvador);
+   printf("digite a populacao2: \n");
+   scanf(" %d" , 2.418.000);
+   printf("digite o pib2: \n");
+   scanf(" %.2f" , 63.526.092,00);  
+   printf("digite os pontos turisticos2: \n");
+   scanf(" %d" , 30);
+
+   
+   
+      
        
 
     return 0;
